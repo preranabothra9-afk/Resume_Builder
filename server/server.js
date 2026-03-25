839e75e4786eb4faaa48a43a07289c49d0af06b4
preranabothra9-afk <preranabothra9@gmail.com>
2026-03-25 23:42:51 +0530

    fixed commit

--- server/server.js
+++ server/server.js
@@ -5,20 +5,17 @@ import connectDB from './configs/db.js';
 import router from './Routes/user.Routes.js';
 import resumeRouter from './Routes/resume.Routes.js';
 import aiRouter from './Routes/ai.Routes.js';
-import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(cors({
-  origin: "*"
-}));
 
 // Database connection
 await connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 
 app.get('/', (req, res) =>{
     res.send("Server is Live...")
